29534fe0c5cbc45ada0e201a33ffdb7b9be78d23
Matt Denny <matthewjdenny@gmail.com>
2015-10-10 13:43:13 -0400

    filled in documentation and skeleton

--- R/generate_document_term_matrix.R
+++ R/generate_document_term_matrix.R
@@ -1,10 +1,11 @@
-#' A function
+#' A function to generate a document term matri from a list of document term vectors.
 #'
-#' @param
+#' @param document_word_list A list of term vectors, one per document, that we wish to turn into a document term matrix.
+#' @param vocabulary An optional vocabulary vector which will be used to form the document term matrix. Defaults to NULL, in which case a vocabulary vector will be generated internally.
 #' @return A list object.
 #' @export
 generate_document_term_matrix <- function(document_word_list,
-                                          unique_words = NULL){
+                                          vocabulary = NULL){
 
 
 
