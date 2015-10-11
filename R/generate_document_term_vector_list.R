bbaabd6dab7cac370f8d5997f561758f9c6a7353
Matt Denny <matthewjdenny@gmail.com>
2015-10-11 16:16:50 -0400

    skeleton of function, need to fill in

---
+++ R/generate_document_term_vector_list.R
@@ -0,0 +1,11 @@
+#' A function to generate a document term vector list in a standard format and save it to file.
+#'
+#' @param input A list of term vectors, one per document, that we wish to save.
+#' @param return_result A logical value indicating whether the resulting list object should be returned.
+#' @return A document term vector list.
+#' @export
+generate_document_term_vector_list <- function(input,
+                                               return_result = FALSE){
+
+    return(NULL)
+}
