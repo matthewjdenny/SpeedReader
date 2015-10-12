472ca0e44cee2fa6f22f2f34a00ce723ecbcf15d
Matt Denny <matthewjdenny@gmail.com>
2015-10-11 22:16:30 -0400

    function to calculate mutual information repeatedly and efficiently on large sparse joint distributions

---
+++ src/Fast_Mutual_Information.cpp
@@ -0,0 +1,35 @@
+#include <RcppArmadillo.h>
+#include <cmath>
+//[[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+double Fast_Mutual_Information(
+        arma::mat joint_dist,
+        arma::vec non_zero_cols
+){
+    // need to modify to take sparse matrix objects
+    //takes an already-normalized matrix
+    double mutual_information = 0;
+    int num_rows = joint_dist.n_rows;
+    int num_cols = joint_dist.n_cols;
+    arma::mat colsums = sum(joint_dist,0);
+    arma::mat rowsums = sum(joint_dist,1);
+    for(int i = 0; i < num_rows; ++i){
+        int cur_total = 0;
+        int cur_max = non_zero_cols[i];
+        int j = 0;
+        while(cur_total < cur_max){
+            double temp = log((joint_dist(i,j)/(colsums[j]*rowsums[i])));
+            if(!std::isfinite(temp)){
+                temp = 0;
+            }
+            mutual_information += joint_dist(i,j) * temp;
+            if(joint_dist(i,j) > 0){
+                cur_total += 1;
+            }
+            j +=1;
+        }
+    }
+    return mutual_information;
+}
