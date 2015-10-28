9853169061f57ae71ce30b4fe63710ced9943df5
Matt Denny <matthewjdenny@gmail.com>
2015-10-28 11:06:50 -0400

    document datasets

---
+++ R/Data_Documentation.R
@@ -0,0 +1,25 @@
+#' Document Term Vector List: Conressional Bills
+#'
+#' A list of five document term vectors drawn from a sample of bills introduced
+#' in congress in the 1980's. These only represent unique terms but are meant to
+#' serve as an illustration.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name document_term_vector_list
+#' @usage data(document_term_vector_list)
+#' @format A list object with 5 fields.
+NULL
+
+#' Document Term Count List: Conressional Bills
+#'
+#' A list of five document term count vectors drawn from a sample of bills introduced
+#' in congress in the 1980's. These represent the ounts of unique terms in the
+#' document_term_vector_list object.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name document_term_count_list
+#' @usage data(document_term_count_list)
+#' @format A list object with 5 fields.
+NULL
