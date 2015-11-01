7a019aa2b7407da57b1d4ce269bb76983f23cbe9
Matt Denny <matthewjdenny@gmail.com>
2015-11-01 16:52:39 -0500

    added two raw text files for testing

--- R/get_file_paths.R
+++ R/get_file_paths.R
@@ -1,9 +1,9 @@
 #' A function the returns the file paths to two example raw datasets for testing
 #'
-#' @param source Can be either "bill tsvs" in which case the paths to the five example bill tsv files are returned, or "test sparse doc-term", in which case the paths to two Rdata files necessary to test the generate_sparse_large_document_term_matrix() are returned.
+#' @param source Can be either "bill tsvs" in which case the paths to the five example bill tsv files are returned, "test sparse doc-term", in which case the paths to two Rdata files necessary to test the generate_sparse_large_document_term_matrix() are returned, or "raw text", in which case the file paths to two example raw text files are provided.
 #' @return A vector of file paths
 #' @export
-get_file_paths <- function(source = c("bill tsvs","test sparse doc-term")){
+get_file_paths <- function(source = c("bill tsvs","test sparse doc-term","raw text")){
     source <- source[1]
     if(source == "bill tsvs"){
         f1 <- system.file("extdata", "bill1.phrases.tsv", package = "SpeedReader")[1]
@@ -17,6 +17,12 @@ get_file_paths <- function(source = c("bill tsvs","test sparse doc-term")){
     if(source == "test sparse doc-term"){
         f1 <- system.file("extdata", "Block_1.Rdata", package = "SpeedReader")[1]
         f2 <- system.file("extdata", "Block_2.Rdata", package = "SpeedReader")[1]
+
+        files <- c(f1,f2)
+    }
+    if(source == "raw text"){
+        f1 <- system.file("extdata", "speech_1.txt", package = "SpeedReader")[1]
+        f2 <- system.file("extdata", "speech_2.txt", package = "SpeedReader")[1]
 
         files <- c(f1,f2)
     }
