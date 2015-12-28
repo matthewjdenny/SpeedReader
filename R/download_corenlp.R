6b2e70f3a632c4869d2e312c64537cc91d06a48f
Matt Denny <matthewjdenny@gmail.com>
2015-12-28 00:55:56 -0500

    tests to make sure that file downloads work

--- R/download_corenlp.R
+++ R/download_corenlp.R
@@ -1,14 +1,14 @@
 #' Checks the java version on your computer and downloads Stanford CoreNLP jar files for use with this package.
 #'
-#' @param version THe version of Core-NLP to download. Defaults to '3.5.2'
+#' @param version The version of Core-NLP to download. Defaults to '3.5.2'
 #' @return Does not return anything.
 #' @export
 download_corenlp <- function(version = "3.5.2"){
     # determine if the user has a new enough version of Java (1.8 or higher)
-    version <- system("java -version", intern = TRUE)
+    system("java -version", intern = TRUE)
 
     Version_OK <- function(){
-        x <- readline(prompt = "Is the java version greater than or equal to  1.8? (yes/no)")
+        x <- readline(prompt = "Is the java version greater than or equal to  1.8? (yes/no): ")
         x <- as.character(x)
         if(is.na(x)){
             stop("Please provide a valid string.")
@@ -41,5 +41,15 @@ download_corenlp <- function(version = "3.5.2"){
     # download the two jar files associated with the selected version
     download.file(url = web1, destfile = f1, method = "auto")
     download.file(url = web2, destfile = f2, method = "auto")
+
+    #check to see that the download worked
+    test1 <- system.file("extdata",paste("stanford-corenlp-",version,".jar",sep = ""), package = "SpeedReader")[1]
+    test2 <- system.file("extdata",paste("stanford-corenlp-",version,"-models.jar",sep = ""), package = "SpeedReader")[1]
+
+    if(test1 != "" & test2 != ""){
+        cat("JAR file downloads appear to have been successful!\n")
+    }else{
+        stop("It appears that one or more of the files did not download successfully...\n")
+    }
 }
 
---
+++ tests/testthat/test_corenlp.R
@@ -0,0 +1,8 @@
+library(SpeedReader)
+context("CoreNLP.")
+
+test_that("CoreNLP Works", {
+
+    download_corenlp(version = "3.5.2")
+
+})
