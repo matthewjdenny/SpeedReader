202b09516d8b8c60fe6e0e2375fd4cc34078282f
Matt Denny <matthewjdenny@gmail.com>
2015-12-28 23:06:12 -0500

    fixed list indexing issue

--- R/corenlp.R
+++ R/corenlp.R
@@ -137,10 +137,10 @@ corenlp <- function(documents = NULL,
         token_counter <- 1
         for(j in 1:length(xml_data)){
             for(k in 1:length(xml_data[[j]])){
-                token_data$word[token_counter] <- xml_data[[j]][[k]]$word
-                token_data$lemma[token_counter] <- xml_data[[j]][[k]]$lemma
-                token_data$POS[token_counter] <- xml_data[[j]][[k]]$POS
-                token_data$NER[token_counter] <- xml_data[[j]][[k]]$NER
+                token_data$word[token_counter] <- xml_data[[j]][[k]]$token$word
+                token_data$lemma[token_counter] <- xml_data[[j]][[k]]$token$lemma
+                token_data$POS[token_counter] <- xml_data[[j]][[k]]$token$POS
+                token_data$NER[token_counter] <- xml_data[[j]][[k]]$token$NER
                 token_data$sentence[token_counter] <- j
                 token_counter <- token_counter + 1
             }
