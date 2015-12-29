dd8ead2aaffc47fe0fb53e87a9893355f8c52889
Matt Denny <matthewjdenny@gmail.com>
2015-12-28 22:54:45 -0500

    update test

--- tests/testthat/test_corenlp.R
+++ tests/testthat/test_corenlp.R
@@ -3,7 +3,15 @@ context("CoreNLP.")
 
 test_that("CoreNLP Works", {
 
+    skip_on_cran()
+    skip("Requires Huge download...")
     # dont run since this is too large of a file
-    #download_corenlp(version = "3.5.2")
+    download_corenlp(version = "3.5.2")
+
+    directory <- system.file("extdata", package = "SpeedReader")[1]
+    Tokenized <- corenlp(
+        documents = NULL,
+        document_directory = directory,
+        version = "3.5.2")
 
 })
