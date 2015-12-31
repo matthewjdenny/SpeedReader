b8e9ced366f2bc47f6ada344cda3ae7c5f433ccc
Matt Denny <matthewjdenny@gmail.com>
2015-12-30 23:56:18 -0500

    preliminary test for mallet lda

---
+++ tests/testthat/test_mallet_lda.R
@@ -0,0 +1,33 @@
+library(SpeedReader)
+context("MALLET LDA.")
+
+test_that("MALLET LDA Works", {
+
+    skip_on_cran()
+    skip("Requires Huge download...")
+
+    files <- get_file_paths(source = "test sparse doc-term")
+
+    sdtm <- generate_sparse_large_document_term_matrix(
+        file_list = files,
+        file_directory = NULL,
+        vocabulary = NULL,
+        maximum_vocabulary_size = -1,
+        using_document_term_counts = TRUE)
+
+    test_results <- mallet_lda(documents = sdtm,
+                           document_directory = NULL,
+                           vocabulary = NULL,
+                           topics = 10,
+                           iterations = 1000,
+                           alpha = 1,
+                           beta = 0.01,
+                           hyperparameter_optimization_interval = 5,
+                           num_top_words = 20,
+                           optional_arguments = "",
+                           tokenization_regex = '[\\p{L}\\p{N}\\p{P}]+',
+                           cores = 1,
+                           delete_intermediate_files = FALSE)
+
+
+})
