0549b259d93752e1938b4e3f86d24753f026cb5a
Matt Denny <matthewjdenny@gmail.com>
2016-01-03 16:10:12 -0500

    don't require java version prompt

--- R/download_corenlp.R
+++ R/download_corenlp.R
@@ -6,28 +6,7 @@
 download_corenlp <- function(version = "3.5.2"){
     # determine if the user has a new enough version of Java (1.8 or higher)
     system("java -version", intern = TRUE)
-
-    Version_OK <- function(){
-        x <- readline(prompt = "Is the java version greater than or equal to  1.8? (yes/no): ")
-        x <- as.character(x)
-        if(is.na(x)){
-            stop("Please provide a valid string.")
-        }
-        ok <- FALSE
-        if(tolower(x) == "yes"){
-            ok <- TRUE
-        }else if(tolower(x) == "no"){
-
-        }else{
-            stop("You must provide an answer of either 'yes' or 'no'.")
-        }
-        return(ok)
-    }
-
-    ok <- Version_OK()
-    if(!ok){
-        stop("You must have java version 1.8 or higher installed on your computer. Please update your java version by visiting the following website and then retry download. Website: http://www.oracle.com/technetwork/java/javase/downloads/index.html -- Make sure to select the JDK option from this page and then download the newest version.")
-    }
+    cat("You must have java version 1.8 or higher installed on your computer. You may update your java version by visiting the following website and then retry download. Website: http://www.oracle.com/technetwork/java/javase/downloads/index.html -- Make sure to select the JDK option from this page and then download the newest version.")
 
     # get the right file names
     directory <- system.file("extdata", package = "SpeedReader")[1]
