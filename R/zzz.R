8010caa195f602cd5f148637b3f9c75d0bd2c2bd
Matt Denny <matthewjdenny@gmail.com>
2016-01-04 02:43:48 -0500

    added startup message

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage("High Performance Text Processing in R.
+https://github.com/matthewjdenny/SpeedReader
+Author: Matt Denny <mdenny@psu.edu>")
+}
