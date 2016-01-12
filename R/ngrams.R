68d9c1a32d2c500bff5c7c5de83f090762338294
Matt Denny <matthewjdenny@gmail.com>
2016-01-11 22:20:29 -0500

    start fleshing out function

--- R/ngrams.R
+++ R/ngrams.R
@@ -24,20 +24,15 @@
 #' John S., and Slava M. Katz. "Technical terminology: some linguistic properties
 #' and an algorithm for identification in text." Natural language engineering
 #' 1.01 (1995): 9-27. Available: https://brenocon.com/JustesonKatz1995.pdf.
-#' The POS tag patterns used are: AN, NN, AAN, ANN, NAN, NNN, NPN. Note that
-#' this method requires input produced by the corenlp() or corenlp_blocked()
-#' functions.
+#' The POS tag patterns used are: AN, NN, AAN, ANN, NAN, NNN, NPN.
 #' @param verb_filtering Defaults to FALSE. If TRUE, then short verb phrases
 #' will be extracted in a manner similar to that described in JK_filtering
-#' above. The POS tag patterns used are: VN, VAN, VNN, VPN, ANV, VDN. Note that
-#' this method requires input produced by the corenlp() or corenlp_blocked()
-#' functions.
+#' above. The POS tag patterns used are: VN, VAN, VNN, VPN, ANV, VDN.
 #' @param phrase_extraction Defaults to FALSE. If TRUE, then full phrases of
 #' arbitrary length will be extracted following the procedure described in Denny,
 #' O'Connor, and Wallach (2016). This method will produce the most phrases, of
 #' highest quality, but will take significantly longer than other methods. Not
-#' currently implemented. Note that this method requires input produced by the
-#' corenlp() or corenlp_blocked() functions.
+#' currently implemented.
 #' @param return_tag_patterns Defaults to FALSE. If TRUE and either JK_filtering
 #' = TRUE, verb_filtering = TRUE, or phrase_extraction = TRUE, then the tag
 #' pattern matched in forming the n-gram/phrase will be returned as an
@@ -66,25 +61,27 @@ ngrams <- function(tokenized_documents = NULL,
 
     # save the current working directory
     currentwd <- getwd()
-    NGrams <- NULL
 
-
+    # check to make sure the user knows what they are extracting
+    EXTRACT_NGRAMS <- TRUE
+    if(is.null(ngram_lengths)){
+        EXTRACT_NGRAMS <- FALSE
+    }
 
     USING_EXTERNAL_FILES <- FALSE
     #check to make sure that we have the right kind of input
     if(!is.null(tokenized_documents) & is.null(tokenized_documents_directory)){
-        if(class(tokenized_documents) != "list" & class(tokenized_documents) != "character"){
-            stop("You must provide a 'documents' object as either a vector of strings (one per document) or a list of string vectors (one entry per document)...")
+        if(class(tokenized_documents) != "list"){
+            stop("You must provide a 'tokenized_documents' object as a list of dataframes produced by the corenlp() or corenlp_blocked() functions...")
         }
-
     }else if(is.null(tokenized_documents) & !is.null(tokenized_documents_directory)){
         setwd(tokenized_documents_directory)
         USING_EXTERNAL_FILES <- TRUE
     }else{
-        stop("You must specify either a valid documents object or a valid document_directory directory path (but not both)...")
+        stop("You must specify either a valid tokenized_documents object or a valid tokenized_documents_directory directory path (but not both)...")
     }
 
-
+    # get all of the file names
     substrRight <- function(x, n){
         substr(x, nchar(x)-n+1, nchar(x))
     }
@@ -92,25 +89,113 @@ ngrams <- function(tokenized_documents = NULL,
     # prepare text to be used
     if(USING_EXTERNAL_FILES){
         if(is.null(file_list)){
-            documents <- list.files()
+            tokenized_documents <- list.files()
             #only use files with a .txt ending
-            endings <- as.character(sapply(documents,substrRight,6))
-            txtfiles <- which(endings == ".Rdata")
-            if(length(txtfiles) > 0){
-                documents <- documents[txtfiles]
+            endings <- as.character(sapply(tokenized_documents,substrRight,6))
+            files <- which(endings == ".Rdata")
+            if(length(files) > 0){
+                tokenized_documents <- tokenized_documents[files]
             }else{
                 stop("Did not find any valid .Rdata files in the specified directory...")
             }
         }else{
-            documents <- file_list
+            tokenized_documents <- file_list
         }
     }
 
-    numdocs <- length(documents)
-    Processed_Text <- vector(length = numdocs, mode = "list")
-    #names(Processed_Text) <- documents
-    filenames <- rep("",numdocs)
+    numdocs <- length(tokenized_documents)
+    NGrams <- NULL
 
+    # now do actual extractions
+    if(USING_EXTERNAL_FILES){
+        Processed_Text <- NULL
+        # if we are using external files, generate ngrams for the curent block,
+        # then save them to disk
+        for(i in 1:numdocs){
+            #load the POS tagged data
+            load(tokenized_documents[i])
+            cur_numdocs <- length(Processed_Text)
+            # create list object to store results
+            NGrams  <- vector(length = cur_numdocs, mode = "list")
+            for(j in 1:cur_numdocs){
+                # initialize list object
+                current <- list()
+                current$ngrams <- NULL
+                current$jk_filtered <- NULL
+                current$verb_filtered <- NULL
+                current$phrases <- NULL
+                current$ngram_lengths <- ngram_lengths
+                current$remove_punctuation <- remove_punctuation
+                current$remove_numeric <- remove_numeric
+
+                if(EXTRACT_NGRAMS){
+                    current$ngrams <- extract_ngrams(
+                        tokenized_document = tokenized_documents[[j]],
+                        ngram_lengths = ngram_lengths,
+                        remove_punctuation = remove_punctuation,
+                        remove_numeric = remove_numeric)
+                }
+                if(JK_filtering){
+                    current$jk_filtered <- extract_jk(
+                        tokenized_document = tokenized_documents[[j]]
+                    )
+                }
+                if(verb_filtering){
+                    current$verb_filtered <- extract_verbs(
+                        tokenized_document = tokenized_documents[[j]]
+                    )
+                }
+                if(phrase_extraction){
+                    # current$phrases <- extract_phrases(
+                    #     tokenized_document = tokenized_documents[[j]]
+                    # )
+                }
+                # save everthing into the list object
+                NGrams[[j]] <- current
+            }
+            save(NGrams,file = paste("NGram_Extractions_",i,".Rdata"))
+        }
+    }else{
+        # if we are using internal data, generate ngrams for all documents and
+        # return list object
+        NGrams  <- vector(length = numdocs, mode = "list")
+        for(i in 1:numdocs){
+            # initialize list object
+            current <- list()
+            current$ngrams <- NULL
+            current$jk_filtered <- NULL
+            current$verb_filtered <- NULL
+            current$phrases <- NULL
+            current$ngram_lengths <- ngram_lengths
+            current$remove_punctuation <- remove_punctuation
+            current$remove_numeric <- remove_numeric
+
+            if(EXTRACT_NGRAMS){
+                current$ngrams <- extract_ngrams(
+                    tokenized_document = tokenized_documents[[i]],
+                    ngram_lengths = ngram_lengths,
+                    remove_punctuation = remove_punctuation,
+                    remove_numeric = remove_numeric)
+            }
+            if(JK_filtering){
+                current$jk_filtered <- extract_jk(
+                    tokenized_document = tokenized_documents[[i]]
+                )
+            }
+            if(verb_filtering){
+                current$verb_filtered <- extract_verbs(
+                    tokenized_document = tokenized_documents[[i]]
+                )
+            }
+            if(phrase_extraction){
+                # current$phrases <- extract_phrases(
+                #     tokenized_document = tokenized_documents[[i]]
+                # )
+            }
+            # save everthing into the list object
+            NGrams[[i]] <- current
+        }
+    }
 
     cat("Completed running N-GRam extraction at:",toString(Sys.time()),"\n")
     # reset the working directory
