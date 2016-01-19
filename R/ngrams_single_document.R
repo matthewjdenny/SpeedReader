5e1e53964311edd4b688067582c95eeafdec23b7
Matt Denny <matthewjdenny@gmail.com>
2016-01-19 12:05:32 -0500

    check to make sure the document is not NULL and report if it is (also save to returned object)

--- R/ngrams_single_document.R
+++ R/ngrams_single_document.R
@@ -21,37 +21,45 @@ ngrams_single_document <- function(j,
     current$remove_numeric <- remove_numeric
     current$lemmatize <- lemmatize
     current$lowercase <- lowercase
+    current$null_document <- FALSE
 
-    if (EXTRACT_NGRAMS) {
-        current$ngrams <- extract_ngrams(
-            tokenized_document = tokenized_document,
-            ngram_lengths = ngram_lengths,
-            remove_punctuation = remove_punctuation,
-            remove_numeric = remove_numeric,
-            lemmatize = lemmatize,
-            lowercase = lowercase)
-    }
-    if (JK_filtering) {
-        current$jk_filtered <- extract_jk(
-            tokenized_document = tokenized_document,
-            lemmatize = lemmatize,
-            lowercase = lowercase
-        )
-    }
-    if (verb_filtering) {
-        current$verb_filtered <- extract_verbs(
-            tokenized_document = tokenized_document,
-            lemmatize = lemmatize,
-            lowercase = lowercase
-        )
-    }
-    if (phrase_extraction) {
-        stop("phrase_extraction not currently implemented")
-        # current$phrases <- extract_phrases(
-        #     tokenized_document = tokenized_document,
-        #     lemmatize = lemmatize,
-        #     lowercase = lowercase
-        # )
+    if (is.null(tokenized_document)) {
+        warning(
+            paste("NULL document provided! Please check input! Document index:",
+                j))
+        current$null_document <- TRUE
+    } else {
+        if (EXTRACT_NGRAMS) {
+            current$ngrams <- extract_ngrams(
+                tokenized_document = tokenized_document,
+                ngram_lengths = ngram_lengths,
+                remove_punctuation = remove_punctuation,
+                remove_numeric = remove_numeric,
+                lemmatize = lemmatize,
+                lowercase = lowercase)
+        }
+        if (JK_filtering) {
+            current$jk_filtered <- extract_jk(
+                tokenized_document = tokenized_document,
+                lemmatize = lemmatize,
+                lowercase = lowercase
+            )
+        }
+        if (verb_filtering) {
+            current$verb_filtered <- extract_verbs(
+                tokenized_document = tokenized_document,
+                lemmatize = lemmatize,
+                lowercase = lowercase
+            )
+        }
+        if (phrase_extraction) {
+            stop("phrase_extraction not currently implemented")
+            # current$phrases <- extract_phrases(
+            #     tokenized_document = tokenized_document,
+            #     lemmatize = lemmatize,
+            #     lowercase = lowercase
+            # )
+        }
     }
     return(current)
 }
