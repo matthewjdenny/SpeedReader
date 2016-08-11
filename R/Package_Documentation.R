c0209b6ed3572769c5c407f98aca15fff096dba9
Matt Denny <matthewjdenny@gmail.com>
2016-08-10 21:51:15 -0400

    add imports to fix notes

--- R/Package_Documentation.R
+++ R/Package_Documentation.R
@@ -1,7 +1,7 @@
 #' SpeedReader: functions to facilitate high performance text processing in R.
 #'
 #' @section SpeedReader functions:
-#' To use this package, You will first want to check out the generate_document_term_vectors() function which will take raw data and transform it into document term vectors. You will then likely want to generate a document term matri using either generate_document_term_matrix() or generate_sparse_large_document_term_matrix() depending on the number of documents you are dealing with. After that, this package provides a number of other functions for simple pointwise mututal information based analysis as will eventualy link to the MALLET libraries for topic modeling with billions of tokens.
+#' To use this package, You will first want to check out the generate_document_term_vectors() function which will take raw data and transform it into document term vectors. You will then likely want to generate a document term matri using either generate_document_term_matrix() or generate_sparse_large_document_term_matrix() depending on the number of documents you are dealing with. After that, this package provides a number of other functions for simple pointwise mututal information based analysis and links to the MALLET libraries for topic modeling with billions of tokens.
 #'
 #' @docType package
 #' @name SpeedReader
@@ -20,9 +20,17 @@ NULL
 #' @import snowfall
 NULL
 
-#' @importFrom grDevices dev.off pdf rgb
+#' @importFrom grDevices dev.off pdf rgb gray
 NULL
 
+#' @importFrom graphics axis legend matplot mtext par plot points segments text
+NULL
+
+#' @importFrom stats cor qnorm
+NULL
+
+#' @importFrom utils download.file head read.delim read.table str write.table
+NULL
 
 #' @useDynLib SpeedReader
 #' @importFrom Rcpp sourceCpp
