9a61ca6e4653896863aa8dc6d025947b411be179
Matt Denny <matthewjdenny@gmail.com>
2016-10-01 23:24:34 -0400

    deal with single token line

--- src/LineWise_Dice_Coefficients.cpp
+++ src/LineWise_Dice_Coefficients.cpp
@@ -18,10 +18,13 @@ arma::mat LineWise_Dice_Coefficients(
         std::vector<std::string> bigrams_1 = line1;
         //take out the first element since we will have one less bigram than
         //unigrams
-        bigrams_1.erase(bigrams_1.begin());
-        //generate bigrams
-        for(int k = 0; k < bigrams_1.size(); ++k){
-            bigrams_1[k] = line1[k] + line1[k+1];
+
+        //generate bigrams only if the line has more than one term
+        if( bigrams_1.size() > 1) {
+            bigrams_1.erase(bigrams_1.begin());
+            for(int k = 0; k < bigrams_1.size(); ++k){
+                bigrams_1[k] = line1[k] + line1[k+1];
+            }
         }
         for(int j = 0; j < number_of_lines2; ++j){
             std::vector<std::string> line2 = Lines2[j];
@@ -29,10 +32,12 @@ arma::mat LineWise_Dice_Coefficients(
             std::vector<std::string> bigrams_2 = line2;
             //take out the first element since we will have one less bigram than
             //unigrams
-            bigrams_2.erase(bigrams_2.begin());
-            //generate bigrams
-            for(int k = 0; k < bigrams_2.size(); ++k){
-                bigrams_2[k] = line2[k] + line2[k+1];
+            if( bigrams_2.size() > 1) {
+                bigrams_2.erase(bigrams_2.begin());
+                //generate bigrams
+                for(int k = 0; k < bigrams_2.size(); ++k){
+                    bigrams_2[k] = line2[k] + line2[k+1];
+                }
             }
             // figure out which bigrams are in both sets
             double both_count = 0;
