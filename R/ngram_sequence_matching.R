a1855783a6a480de72211783a31859cb000447b0
Matt Denny <matthewjdenny@gmail.com>
2016-12-02 00:42:30 -0500

    more sequence matching stats

--- R/ngram_sequence_matching.R
+++ R/ngram_sequence_matching.R
@@ -28,10 +28,47 @@ ngram_sequence_matching <- function(document_1,
                                                document_2,
                                                ngram_size)
 
+    # we can also calculate some metrics on these like the average length of
+    # contiguous match sequence
+    m_seq_1 <- NULL
+    cur_match <- 0
+    ms <- res[[1]]
+    for (i in 1:length(ms)) {
+        if (ms[i] == 1) {
+            cur_match <- cur_match + 1
+        } else {
+            if (cur_match != 0) {
+                m_seq_1 <- c(m_seq_1,cur_match)
+                cur_match <- 0
+            }
+        }
+    }
+
+    m_seq_2 <- NULL
+    cur_match <- 0
+    ms <- res[[2]]
+    for (i in 1:length(ms)) {
+        if (ms[i] == 1) {
+            cur_match <- cur_match + 1
+        } else {
+            if (cur_match != 0) {
+                m_seq_2 <- c(m_seq_2,cur_match)
+                cur_match <- 0
+            }
+        }
+    }
+
+    mss <- data.frame(num_match_blocks = c(length(m_seq_1),length(m_seq_2)),
+                      max_block_length = c(max(m_seq_1),max(m_seq_2)),
+                      min_block_length = c(min(m_seq_1),min(m_seq_2)),
+                      mean_block_length = c(mean(m_seq_1),mean(m_seq_2)),
+                      median_block_length = c(median(m_seq_1),median(m_seq_2)),
+                      total_blocks = c(length(res[[1]]), length(res[[2]])))
 
     result <- list(matches_document_1 = res[[1]],
                    matches_document_2 = res[[2]],
-                   ngram_length = ngram_size)
+                   ngram_length = ngram_size,
+                   match_sequence_statistics = mss)
     t2 <- proc.time() - ptm
     cat("Complete in:",t2[[3]],"seconds...\n")
     return(result)
