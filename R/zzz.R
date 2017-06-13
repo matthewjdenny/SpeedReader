1b913cf891cadd8031d7555523a5381128f32cb6
Matt Denny <matthewjdenny@gmail.com>
2017-06-13 09:48:52 -0400

    version bump

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage("SpeedReader: High Performance Text Processing Libraries
-Version 0.6.1 created on 2016-12-11.
-copyright (c) 2015-2016, Matthew J. Denny, Penn State University
+Version 0.7.0 created on 2017-06-13.
+copyright (c) 2015-2017, Matthew J. Denny, Penn State University
 Type help('SpeedReader') to get started.
 Development website: github.com/matthewjdenny/SpeedReader
 Contact: mdenny@psu.edu")
