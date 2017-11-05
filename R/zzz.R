d74e4135a475703827e4212e72d22ec12c4e5d0a
Matt Denny <matthewjdenny@gmail.com>
2017-11-04 21:51:01 -0400

    update to package description

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-    packageStartupMessage("SpeedReader: High Performance Text Processing Libraries
-Version 0.8.0 created on 2017-10-25.
+    packageStartupMessage("SpeedReader: High Performance Text Analysis
+Version 0.8.1 created on 2017-11-04.
 copyright (c) 2015-2017, Matthew J. Denny, Penn State University
 Type help('SpeedReader') to get started or check out the vignette with:
 vignette('getting_started_with_SpeedReader')
